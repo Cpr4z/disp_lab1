c259662cac12a0bacdd3ab9711f967c4be63650b
cpr4z <matvey.agarkov02@gmail.com>
2024-11-23 13:21:20 +0300

    fsfsf

--- src/app/web_app/urls.py
+++ src/app/web_app/urls.py
@@ -2,6 +2,6 @@ from django.urls import path, include, re_path
 from . import views
 
 urlpatterns = [
-    re_path(r'^api/v1/persons$', views.web_app),
-    re_path(r'^api/v1/persons/([0-9]+)$', views.web_app),
+    re_path(r'^api/v1/persons$', views.persons_api),
+    re_path(r'^api/v1/persons/([0-9]+)$', views.persons_api),
 ]
